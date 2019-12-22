2fe60d8c06d88c09f0a4071e75dee822f71aeb0d
DuArms <58860510+DuArms@users.noreply.github.com>
2019-12-22 12:58:22 +0200

    hai cu funtia fa

--- Euristic.cpp
+++ Euristic.cpp
@@ -1,1 +1,6 @@
 #include "Euristic.h"
+bool Euristic::alegem_un_fraier(double T, double a, double b)
+{
+	//	std::cout << a << " " << b;
+	return (static_cast<double>(rand()) / (static_cast<double>(RAND_MAX))) < exp((double(-1 * abs(b - a)) / T));
+}
--- Euristic.h
+++ Euristic.h
@@ -2,6 +2,7 @@
 #include "Individ.h"
 class Euristic
 {
+	bool alegem_un_fraier(double T, double a, double b);
 public:
 	Individ cobai;
 	void (*functie_cu_valoare)(Graf, int);
@@ -21,6 +22,56 @@ public:
 		}
 		std::cout << count;
 		return 0;
+	}
+
+	void simulated_annealing(Graf *g)
+	{
+		double* raspuns = new double[g->graf.size()];
+		double minim_global = 10000000, minim_local;
+		Individ vecin(g);
+		double temperatura = 1000.0;
+		Individ candidat(g);
+		const unsigned int dimensiuni= g->graf.size();
+		
+		candidat.trasaturi.initiere_random();
+
+		double minim_curent;// calculeaza_functie(candidat);
+
+		bool stop = false;
+		for (int ii = 0; ii < 120; ++ii)
+			do {
+				int set = 12000;
+				do
+				{
+					vecin = candidat;
+					int length = rand() % (candidat.trasaturi.ADN.size()) + 1;
+					for (int i2 = 0; i2 < length; ++i2)
+					{
+						const int pos = rand() % candidat.trasaturi.ADN.size();
+						vecin.trasaturi.ADN[pos] = !vecin.trasaturi.ADN[pos];
+					}
+					//minim_local = calculeaza_functie(vecin);
+					if (minim_curent > minim_local)
+					{
+						minim_curent = minim_local;
+						candidat = vecin;
+					}
+					else
+						if (alegem_un_fraier(temperatura, minim_curent, minim_local))
+						{
+							minim_curent = minim_local;
+							candidat = vecin;
+						}
+					set--;
+				} while (set);
+				temperatura *= 0.9;
+				if (minim_global > minim_curent)
+				{
+					minim_global = minim_curent;
+					cobai = candidat;
+				}
+			} while (temperatura > 1);
+			delete[] raspuns;
 	}
 	Euristic(Individ a) : cobai(a) {};
 
